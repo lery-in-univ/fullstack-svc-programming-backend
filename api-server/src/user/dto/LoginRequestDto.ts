832a9c2ed5f44b8d5baaddb6597e4b14d443735a
Coalery <doralife12@gmail.com>
2025-11-06 01:08:11 +0900

    fix: 이메일 포맷 확인하도록 수정

--- api-server/src/user/dto/LoginRequestDto.ts
+++ api-server/src/user/dto/LoginRequestDto.ts
@@ -1,8 +1,7 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginRequestDto {
-  @IsNotEmpty()
-  @IsString()
+  @IsEmail()
   email: string;
 
   @IsNotEmpty()
